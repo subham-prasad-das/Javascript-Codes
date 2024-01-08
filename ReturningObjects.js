1489d959a903979c4a6060041359c5b90198f648
subham <dark.demon.das@gmail.com>
2024-01-08 20:52:51 +0530

    Ways to return objects

---
+++ ReturningObjects.js
@@ -0,0 +1,30 @@
+/* Ways to retun objects */
+
+
+/*
+Returning objects through parameters
+
+const person = {
+    name : "Subham",
+    DOY : 2000,
+}
+
+function persona(){
+    return person;
+}
+
+console.log(persona());
+*/
+
+/* Returning objects through arrow functions directly
+
+const persona = () =>
+    ({
+        firstName : "Subham",
+        LastName : "Das"
+    })
+
+console.log(persona());
+
+Here we need to wrap the whole object in side the parenthesis and also inside the braces in order to return it.
+*/
