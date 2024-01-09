462bc6cc6911f805cb798c61a7f94aa5d6d115ae
subham <dark.demon.das@gmail.com>
2024-01-09 19:29:21 +0530

    Ways to destructuring objects

--- destructuringObjects.js
+++ destructuringObjects.js
@@ -5,12 +5,12 @@ const person = {
 
 /*
 Destructuring the objects -->
+const {n, a} = person;
 
+console.log(n); // Subham
+console.log(a); // 24
 
-const {name, age} = person;
-
-console.log(name); // Subham
-console.log(age); // 24
+here it is required to use the keys name as the variables to store the properties
 */
 
 /*
@@ -22,4 +22,5 @@ console.log(personAge); // 24
 
 console.log(person.name);
 
+here it is not required to use the keys name as the variables to store the properties
  */
