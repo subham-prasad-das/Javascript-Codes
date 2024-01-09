bec40a8ee62fdbff2579b28c89df042ba2adcb90
subham <dark.demon.das@gmail.com>
2024-01-09 19:23:12 +0530

    Ways to destructuring objects

---
+++ destructuringObjects.js
@@ -0,0 +1,25 @@
+const person = {
+    name : "Subham",
+    age : 24
+}
+
+/*
+Destructuring the objects -->
+
+
+const {name, age} = person;
+
+console.log(name); // Subham
+console.log(age); // 24
+*/
+
+/*
+Assigning to different variable names
+const { name: personName, age: personAge } = person;
+
+console.log(personName); // Subham
+console.log(personAge); // 24
+
+console.log(person.name);
+
+ */
