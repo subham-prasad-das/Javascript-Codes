3b9a2f41101ff856eb0c06510b7b18440aced266
subham <dark.demon.das@gmail.com>
2024-01-09 20:47:07 +0530

    Restructuring of the object keys and properties from the global scope

---
+++ ObjectLiteralsEnhancement.js
@@ -0,0 +1,12 @@
+// Object literal enhancement is the opposite of destructuring.
+// Here we are having the values first then making an obhect using them.
+
+const name = "Subham";
+const age = 24;
+ 
+/*
+Adding keys and properties to an object through resturcturing or Object Literal Enhancement ----->
+
+const person = {name, age};
+console.log(person); // output --> { name: 'Subham', age: 24 }
+*/
